9746ade0e624251ff791f75ca7498fd21f2033c7
Carollyn Thio <carollynthiow@gmail.com>
2026-05-21 16:13:03 +0700

    added some icons

---
+++ frontend/src/components/icons/CheckIcon.jsx
@@ -0,0 +1,7 @@
+const CheckIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#795950" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <polyline points="20 6 9 17 4 12"/>
+  </svg>
+);
+
+export default CheckIcon;
