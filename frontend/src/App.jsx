51c720a64e425017024c6b611e24a00941954fed
Carollyn Thio <carollynthiow@gmail.com>
2026-05-21 16:20:47 +0700

    Created register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import CreateProduct from './pages/CreateProduct';
 // import ProductDetailsPage from './pages/ProductDetailsPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/sign-in" element={<LoginPage />} />
+                <Route path="/sign-up" element={<RegisterPage />} />
                 {/* <Route path="/" element={<Dashboard />} />
                 <Route path="/create" element={<CreateProduct />} />
                 <Route path="/product/:id" element={<ProductDetailsPage />} /> */}
