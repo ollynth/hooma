9e92c83bcf2cccec962fd68a2fc3ab975da5d14c
Carollyn Thio <carollynthiow@gmail.com>
2026-05-25 11:40:19 +0700

    Updated API

--- frontend/src/middleware/api.js
+++ frontend/src/middleware/api.js
@@ -4,9 +4,22 @@ import axios from 'axios';
 // const API_BASE_URL = import.meta.env.VITE_BE_URL;
 const API_BASE_URL = process.env.REACT_APP_BE_URL;
 
+const api = axios.create({
+    baseURL: API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+})
+
+// Authentication API calls
 export const loginUser = async (identifier, password) => {
     try {
-        const response = await axios.post(`${API_BASE_URL}/auth/login`, { identifier, password });
+        const response = await api.post(`${API_BASE_URL}/auth/login`, { identifier, password });
         return response.data;
     } catch (err) {
         console.log('Backend error:', err.response?.data);
@@ -16,7 +29,7 @@ export const loginUser = async (identifier, password) => {
 
 export const registerUser = async (firstName, lastName, email, password) => {
     try {
-        const response = await axios.post(`${API_BASE_URL}/auth/register`, { firstName, lastName, email, password });
+        const response = await api.post(`${API_BASE_URL}/auth/register`, { firstName, lastName, email, password });
         return response.data;
     } catch (err) {
         console.log('Backend error:', err.response?.data);
@@ -24,21 +37,22 @@ export const registerUser = async (firstName, lastName, email, password) => {
     }
 };
 
+// Product API calls
 export const fetchProducts = async () => {
-    const response = await axios.get(`${API_BASE_URL}/products`);
+    const response = await api.get(`${API_BASE_URL}/products`);
     return response.data;
 };
 
 export const createProduct = async (productData) => {
-    const response = await axios.post(API_BASE_URL+ "/products", productData);
+    const response = await api.post(`${API_BASE_URL}/products`, productData);
     return response.data;
 };
 
 export const deleteProduct = async (productId) => {
-    await axios.delete(`${API_BASE_URL}/products/${productId}`);
+    await api.delete(`${API_BASE_URL}/products/${productId}`);
 };
 
 export const updateProduct = async (productId, updatedData) => {
-    const response = await axios.put(`${API_BASE_URL}/products/${productId}`, updatedData);
+    const response = await api.put(`${API_BASE_URL}/products/${productId}`, updatedData);
     return response.data;
 };
